25dbcae16f46998087a03192be65ab496ee54497
Kevin4Calderon <91550461+Kevin4Calderon@users.noreply.github.com>
2021-11-25 20:10:12 -0600

    Update Funcion-Inversa.m

--- Funcion-Inversa.m
+++ Funcion-Inversa.m
@@ -1,3 +1,11 @@
+% Script de Octave
+% Titulo      :Funcion Inversa 1
+% Author      :Kevin Yoan Calderón García
+% Description :Script para graficar una funciones inversas
+% Date        :20211123
+% Version     :1
+% Usage       :octave> cd /path/
+% Notes       :Requiere aplicacion octave
 clear
 pkg load symbolic
 syms x
