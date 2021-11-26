47497ec8187e0f5d8d27535effeb66adc10a1299
Kevin4Calderon <91550461+Kevin4Calderon@users.noreply.github.com>
2021-11-25 20:12:40 -0600

    Update Funcion-Inversa.m

--- Funcion-Inversa.m
+++ Funcion-Inversa.m
@@ -1,17 +1,28 @@
-% Script de Octave
-% Titulo      :Funcion Inversa 1
-% Author      :Kevin Yoan Calderón García
-% Description :Script para graficar una funciones inversas
-% Date        :20211123
-% Version     :1
-% Usage       :octave> cd /path/
-% Notes       :Requiere aplicacion octave
+¿% Octave Script
+%School                :Tecnologico de Estudios Superiores de Jilotepec 
+%Specialty             :Ingeniería en Sistemas Computacionales 
+% Title                :Funcion Inyectiva, Suprayectiva y Biyectiva
+% Description          :Script del segundo ejercicio de la actividad 18
+% Author               :Victoria Avigail Escobar 
+%                      :Jose Miguel Sanchez
+%                      :Misael Barbosa
+%                      :Kevin Yoan Calderón García
+%                      :Francisco Javier, Diego Armando, Kevin Joan.
+% Date                 :20211125
+%Grupo                 :3102 y 3101
+
 clear
+
 pkg load symbolic
 syms x
 x = -10:1:10
 ix = log (8) .*(x)
+
+
 hold on
+
 plot (x, ix);
+
 grid on
+
 title('Funcion inverse');
